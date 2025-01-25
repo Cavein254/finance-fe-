51b30ff08303f24704fe858192337c7dcd1783e2
johnmuthua <caveomuthama@gmail.com>
2025-01-25 12:17:09 +0300

    feat: add compare function

---
+++ src/components/common/PillButton.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { StockTicker } from "../../__generated__/graphql";
+
+interface PillButtonProps {
+  symbol: StockTicker;
+  handleAddTicker: (arg0: StockTicker) => void;
+  handleRemoveTicker: (arg0: string) => void;
+}
+
+const PillButton = ({
+  symbol,
+  handleAddTicker,
+  handleRemoveTicker,
+}: PillButtonProps) => {
+  const [active, setActive] = useState(false);
+  const handleClick = () => {
+    if (active === false) {
+      setActive(true);
+      handleAddTicker(symbol);
+    } else {
+      handleRemoveTicker(symbol.id);
+      setActive(false);
+    }
+  };
+  return (
+    <button
+      className={`px-2 py-1  text-white rounded-xl text-sm ${
+        active ? "bg-green-600" : "bg-indigo-400"
+      }`}
+      onClick={handleClick}
+    >
+      {symbol.name}
+    </button>
+  );
+};
+
+export default PillButton;
--- src/pages/ticker/compare.tsx
+++ src/pages/ticker/compare.tsx
@@ -1,23 +1,77 @@
-import React from "react";
 import StackedLineChart from "../../components/charts/StackedLineChart";
-import { Pill } from "lucide-react";
 import { useQuery } from "@apollo/client";
 import { GET_ALL_STOCK_SYMBOLS } from "../../graphql/operations/query/ticker";
 import ErrorDisplay from "../../components/common/ErrorDisplay";
+import { useState } from "react";
+import { StockTicker } from "../../__generated__/graphql";
+import PillButton from "../../components/common/PillButton";
+import { Button } from "../../components/ui/button";
 
 const CompareTicker = () => {
+  const [symbols, setSymbols] = useState<Array<StockTicker> | undefined>(
+    undefined
+  );
+  const [selected, setSelected] = useState<Array<StockTicker>>([]);
+
+  const handleAddTicker = (ticker: StockTicker) => {
+    setSelected((prevSelected) => {
+      if (!prevSelected.some((item) => item.id === ticker.id)) {
+        return [...prevSelected, ticker];
+      }
+      return prevSelected;
+    });
+  };
+  const handleRemoveTicker = (id: string) => {
+    setSelected((prevSelected) =>
+      prevSelected.filter((ticker) => ticker.id !== id)
+    );
+  };
+
+  console.log(selected);
+
   const { error } = useQuery(GET_ALL_STOCK_SYMBOLS, {
     onCompleted: (data) => {
-      if (!data?.getAllSymbols.success) {
+      if (data?.getAllSymbols.success) {
+        const udata = data?.getAllSymbols.data;
+        setSymbols(udata);
       }
     },
   });
+  const symbolsList = symbols?.map((symbol) => (
+    <PillButton
+      symbol={symbol}
+      handleAddTicker={handleAddTicker}
+      handleRemoveTicker={handleRemoveTicker}
+    />
+  ));
+  const selectedSymbols = selected?.map((symbol) => (
+    <PillButton
+      symbol={symbol}
+      handleAddTicker={handleAddTicker}
+      handleRemoveTicker={handleRemoveTicker}
+    />
+  ));
   return (
     <div>
       {error && <ErrorDisplay error={JSON.stringify(error)} />}
-      <button className="px-4 py-2 bg-indigo-400 text-white rounded-xl">
-        mfst
-      </button>
+      <div className="px-4">
+        {selected.length > 0 && (
+          <div className="py-6 flex flex-col">
+            <h4 className="font-bold mb-2">
+              You have selected the following symbols to compare:
+            </h4>
+            <div className="flex justify-between">
+              <div className="flex gap-2 flex-wrap">{selectedSymbols}</div>
+              <div className="">
+                <Button>Compare</Button>
+              </div>
+            </div>
+          </div>
+        )}
+        <div className=" dark:bg-slate-600 bg-slate-300 p-2 flex flex-wrap justify-center items-center gap-2 h-[30vh] overflow-scroll">
+          {symbolsList}
+        </div>
+      </div>
       <StackedLineChart />
     </div>
   );
